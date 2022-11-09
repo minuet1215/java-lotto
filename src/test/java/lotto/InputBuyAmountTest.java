502ea29789979214daecd973929eaf7ddf787f00
jungle <minuet1215@jbnu.ac.kr>
2022-11-09 19:10:40 +0900

    Test(InputBuyAmountTest): 구매 금액 테스트 코드 추가

--- src/test/java/lotto/InputBuyAmountTest.java
+++ src/test/java/lotto/InputBuyAmountTest.java
@@ -1,2 +1,15 @@
-package lotto;public class InputBuyAmountTest {
+package lotto;
+
+import lotto.validator.AmountValidator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputBuyAmountTest {
+    @Test
+    void 천으로_나누어_떨어지지_않을_경우() {
+        int amount = 5001;
+        assertThatThrownBy(() -> new AmountValidator().validateDivideBy1000(amount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
