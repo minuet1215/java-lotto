3f53bfaf415fbe3fe6eabb936ba1e5b581bdd0e8
jungle <minuet1215@jbnu.ac.kr>
2022-11-10 02:46:42 +0900

    Refactor(AmountValidator): isDivideBy1000 로직 변경

--- src/main/java/lotto/validator/AmountValidator.java
+++ src/main/java/lotto/validator/AmountValidator.java
@@ -1,25 +1,26 @@
 package lotto.validator;
 
 public class AmountValidator {
+    public void validateInput(String amount) {
+        validateInputAmount(amount);
+        validateDivideBy1000(Integer.parseInt(amount));
+    }
+
     public void validateInputAmount(String amount) {
         try {
             Integer.parseInt(amount);
         } catch (Exception e) {
-            System.out.println("[ERROR]");
+            throw new IllegalArgumentException();
         }
     }
 
     public void validateDivideBy1000(int amount) {
-        if (isDivideBy1000(amount)) {
+        if (!isDivideBy1000(amount)) {
             throw new IllegalArgumentException();
         }
     }
 
     private boolean isDivideBy1000(int amount) {
-        if (amount % 1000 != 0) {
-            return true;
-        }
-        return false;
+        return amount % 1000 == 0;
     }
 }
-
